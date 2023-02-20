079e7c310b99e56692f2b3288d5d3e72b32a0e78
Danielle McIntosh <118628680+invtrdan@users.noreply.github.com>
2023-02-20 02:04:56 -0500

    Update and rename fibonacci_sequence.py to print_fibonacci_sequence.py
    fib function prints the first n elements in the Fibonacci sequence.

--- print_fibonacci_sequence.py
+++ print_fibonacci_sequence.py
@@ -1,4 +1,4 @@
-def recursive_fib(num, num1 = 0, num2 = 1, count = 0):
+def fib(num, num1 = 0, num2 = 1, count = 0):
     if count == 0:
         print(num1)
         count += 1
@@ -10,14 +10,14 @@ def recursive_fib(num, num1 = 0, num2 = 1, count = 0):
         temp = num1
         num1 = num2
         num2 = temp + num1
-        return recursive_fib(num, num1, num2, count)
+        return fib(num, num1, num2, count)
     
     
 print("Enter '0' to stop.")
 num = int(input('Enter a number: '))
 while num != 0:
     print('Fibonacci Sequence')
-    recursive_fib(num)
+    fib(num)
     print()
     num = int(input('Enter a number: '))
     
